2e0b8a8a8732c7172bd9207ca55727399e252b7d
allequalit <50491988+allequalit@users.noreply.github.com>
2019-07-01 15:20:12 +0900

    Update do.sh

--- do.sh
+++ do.sh
@@ -14,3 +14,7 @@ sudo service [80번포트를 사용해서 중지했던 프로그램] start
 # 80번포트를 사용해서 중지했던 프로그램을 시작합니다.
 # 80번포트를 사용해서 중지했던 프로그램이 apache인 경우 1번째줄을 service apache2 start 로 대체해주세요. 
 # 80번 포트를 사용해서 중지했던 프로그램이 nginx인 경우 1번째줄을 service nginx start 로 대체해주세요.
+
+sudo cp -r /etc/letsencrypt/live/[자신의 도메인을 입력해주세요] /etc/ssf/ 
+
+# /etc/ssf/ 폴더 생성 및 ssl 인증서를 /etc/ssf 디렉터리로 이동합니다.
