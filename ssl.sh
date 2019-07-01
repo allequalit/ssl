f668dd1c49c37caf8129a976068528c7908c1ccb
allequalit <50491988+allequalit@users.noreply.github.com>
2019-07-01 18:11:01 +0900

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -15,8 +15,11 @@ sudo service [80번포트를 사용하고있는 프로그램] stop
 
 sudo certbot certonly --standalone -d [자신의 도메인을 입력해주세요]
 
-# 예시 1 : 자신의 도메인이 ael.kr인 경우 9번째줄을 certbot certonly -standalone -d ael.kr 로 대체해주세요.
+# 예시 : 자신의 도메인이 ael.kr인 경우 9번째줄을 certbot certonly -standalone -d ael.kr 로 대체해주세요.
 #  Congratulations! Your certificate and chain have been saved at 이란 문구가 표시되면 ssl 인증서가 정상적으로 생성 되었습니다.
+
+# ssl 인증서는 /etc/letsencrypt/live/[자신의 도메인을 입력해주세요]/ 폴더에 있습니다.
+# 예시 : 자신으 도메인이 ael.kr인 경우 /etc/letsencrypt/live/ael.kr/ 폴더에 ssl 인증서가 있습니다.
 
 sudo service [80번포트를 사용해서 중지했던 프로그램] start 
 
@@ -24,9 +27,6 @@ sudo service [80번포트를 사용해서 중지했던 프로그램] start
 # 80번포트를 사용해서 중지했던 프로그램이 apache인 경우 1번째줄을 service apache2 start 로 대체해주세요. 
 # 80번 포트를 사용해서 중지했던 프로그램이 nginx인 경우 1번째줄을 service nginx start 로 대체해주세요.
 
-sudo ln -s /etc/letsencrypt/live/[자신의 도메인을 입력해주세요] /etc/ssf/ 
-
-# /etc/ssf 폴더에 접근하면 ssl 인증서가 있는 폴더로 접근할수있습니다. (심볼릭 링크)
 
 sudo apt-get install cron -y
 
