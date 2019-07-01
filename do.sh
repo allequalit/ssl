413498b02e2fadce7e2e87592d057455b3cb1da3
allequalit <50491988+allequalit@users.noreply.github.com>
2019-07-01 18:07:52 +0900

    Update do.sh

--- do.sh
+++ do.sh
@@ -14,7 +14,3 @@ sudo service [80번포트를 사용해서 중지했던 프로그램] start
 # 80번포트를 사용해서 중지했던 프로그램을 시작합니다.
 # 80번포트를 사용해서 중지했던 프로그램이 apache인 경우 1번째줄을 service apache2 start 로 대체해주세요. 
 # 80번 포트를 사용해서 중지했던 프로그램이 nginx인 경우 1번째줄을 service nginx start 로 대체해주세요.
-
-sudo ln -s /etc/letsencrypt/live/[자신의 도메인을 입력해주세요] /etc/ssf/ 
-
-# /etc/ssf/ 폴더에 접근하면 ssl 인증서가 있는 폴더로 접근할수있습니다. (심볼릭 링크)
